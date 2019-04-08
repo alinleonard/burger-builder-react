84ad0ae7c5941a8eaeb9c20e6819f48853189e3f
alinleonard <balinleonard@gmail.com>
2019-04-08 12:42:37 +0300

    outputting burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,27 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients) // ["salad", "bacon", "cheese", "meat"]
+        .map((ingredientKey) => { // Ex. ingredientKey = "salad"
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key={ingredientKey+i} type={ingredientKey} />;
+            })
+        });
+
+        // let transformedIngredients = [];
+ 
+        // for (const key of Object.keys(props.ingredients)) {
+        //   for(let i = 1; i <= props.ingredients[key]; i++) {
+        //     transformedIngredients.push(
+        //       <BurgerIngredient type={key} key={key + i}/>
+        //     );
+        //   }
+        // }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Auxilary from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Auxilary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Auxilary>
         );
